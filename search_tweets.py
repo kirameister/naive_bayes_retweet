711e89db1f272ff0a8a0241221e68c960efb53c9
1024jp <1024jp@wolfrosch.com>
2014-04-27 18:06:05 +0200

    Style code pythonic

--- search_tweets.py
+++ search_tweets.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
+
 import tweepy
+
 
 # It may be worth having api keys stored in YAML file as well (if it's worth..)
 auth = tweepy.OAuthHandler('consumer_key', 'consumer_secret')
 auth.set_access_token('access_token', 'access_token_secret')
 api = tweepy.API(auth)
 
+
 def main(argquery, arglang):
-    statuses = api.search(argquery, lang=arglang, rpp='100' )
+    statuses = api.search(argquery, lang=arglang, rpp='100')
     for status in statuses:
         status.text = re.sub(r'[\n\t]', ' ', status.text)
         status.user.screen_name = re.sub(r'[\n\r]', ' ', status.user.screen_name)
@@ -18,7 +21,7 @@ def main(argquery, arglang):
         if (status.retweeted == True or re.search('^RT', status.text)):
             # I only want the non-RTed tweets!
             continue
-        # Tweet ID and its content shall be retrieved.. 
+        # Tweet ID and its content shall be retrieved..
         print(str(status.id) + "\t" + status.text)
 
 if __name__ == '__main__':
@@ -27,5 +30,3 @@ if __name__ == '__main__':
         sys.stderr.write("Usage: python " + args[0] + " twitter_query lang_code\n")
         sys.exit()
     main(args[1], args[2])
-
-
